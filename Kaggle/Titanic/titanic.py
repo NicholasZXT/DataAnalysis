c3974501f4f930fc64c17f41f25d56e3a18b41bc
NicholasZXT <1036562425@qq.com>
2020-03-07 09:43:43 +0800

    titanic练习

--- Kaggle/Titanic/titanic.py
+++ Kaggle/Titanic/titanic.py
@@ -2,18 +2,115 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.impute import SimpleImputer
+from sklearn.impute import MissingIndicator
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
 
+
+#查看数据
+# %cd Kaggle/Titanic/
+train = pd.read_csv('train.csv')
+test = pd.read_csv('test.csv')
+# describe只会给出数值型变量的统计信息，没有多少用处
+# train.describe()
+# info()可以看出，age, cabin, embarked 含有缺失值
+train.info()
+test.info()
+
+# 将乘客ID设为index，这个变量对于预测没有用
+train.set_index('PassengerId',inplace=True)
+test.set_index('PassengerId', inplace=True)
+# 选择用于预测的变量
+# cols = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare', 'Cabin']
+selected_features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']
+X = train[selected_features]
+y = train['Survived']
+X_test = test[selected_features]
+
+# 填充缺失值，有两种方式
+# 1. 使用imputer处理会报错，因为含有 非数值的列sex
+# imp_mean = SimpleImputer(strategy='mean')
+# X_imp = imp_mean.fit_transform(X)
+# 2. 更好的方式是使用pandas的缺失值填充方法
+age_fillna_train = X['Age'].mean()
+X['Age'].fillna(value=age_fillna_train, inplace=True)
+# 注意，对于测试数据也要处理缺失值
+age_fillna_test = X_test['Age'].mean()
+X_test['Age'].fillna(value=age_fillna_test, inplace=True)
+# 测试集的Fare还有一个缺失值
+fare_fillna_test = X_test['Fare'].mean()
+X_test['Fare'].fillna(value=fare_fillna_test, inplace=True)
+# 检查是否还有缺失值
+# X.info()
+# X_test.info()
+
+# 因为Sex是一个离散变量，需要进行特征处理，这里有三种方式
+# 1. 使用sklearn.processing的OneHotEncoder
+# 默认会将所有的列都当做离散变量处理，不太好用
+# enc = OneHotEncoder(sparse=False)
+# t = enc.fit_transform(X)
+# enc.get_feature_names()
+# 2. 使用pandas的get_dummies()函数更容易处理
+X_proc = pd.get_dummies(X)
+X_test_proc = pd.get_dummies(X_test)
+# 3. 使用DictVectorizer构造特征
+# from sklearn.feature_extraction import DictVectorizer
+# dict_vec = DictVectorizer(sparse=False)
+# X_proc = dict_vec.fit_transform(X.to_dict(orient='record'))
+# X_test_proc = dict_vec.fit_transform(X_test.to_dict(orient='record'))
+# dict_vec.get_feature_names()
+# X_df = pd.DataFrame(X_proc, columns=dict_vec.get_feature_names())
+
+# 处理离散特征时，上面三种方式都可以使用，但是我偏向于使用get_dummies，
+# 因为另外两种得到的是np.array，没有列名，而使用get_dummies得到的仍然是DF
+
+
+# 开始训练模型
+# Logistic
+#预测正确率为 0.74162
+logic = LogisticRegression()
+logic.fit(X_proc, y)
+logic.classes_
+logic.coef_
+logic.intercept_
+logic.score(X_proc, y)
+y_test = logic.predict(X_test_proc)
+y_test_df = pd.DataFrame(y_test, index= X_test.index)
+y_test_df.columns = ['Survived']
+y_test_df.to_csv("predict_2.csv")
+
+# 使用交叉验证的Logistic,
+log_cv = LogisticRegressionCV(Cs=np.linspace(0.5,1.5,5), cv=5, max_iter=1000)
+log_cv.fit(X_proc, y)
+log_cv.coef_
+y_test = log_cv.predict(X_test_proc)
+y_test_df = pd.DataFrame(y_test, index=X_test_proc.index)
+
+# 使用Random Forest
+rfc = RandomForestClassifier(n_estimators=30, random_state=2)
+rfc.fit(X_proc, y)
+rfc.n_estimators
+rfc.n_features_
+# 看一下泛化能力
+cross_val_score(rfc, X_proc, y, cv=5)
+cross_val_score(rfc, X_proc, y, cv=5).mean()
+# 预测
+y_test = rfc.predict(X_test_proc)
+y_test_df = pd.DataFrame(y_test, index=X_test.index)
+
+
+
+# ---------------------------------------
+# TODO kaggle实战里的代码部分
 dtype = {'PassengerId': str}
 train_all = pd.read_csv("train.csv", dtype=dtype)
 # 根据列索引来删除某一列
 train = train_all.drop(train_all.columns[1], axis=1)
 # train['PassengerId'] = train['PassengerId'].astype(str)
 test = pd.read_csv("test.csv", dtype=dtype)
-
-train_all.shape
-train.shape
-test.shape
-
 selected_features = ['Pclass', 'Sex', 'Age', 'Embarked', 'SibSp', 'Parch', 'Fare']
 X_train = train[selected_features]
 X_test = test[selected_features]
@@ -21,9 +118,6 @@ X_test = test[selected_features]
 # y_train = train_all.iloc[:, [1]]
 # 还是老老实实的使用获取的Series
 y_train = train_all['Survived']
-
-X_train.shape
-y_train.shape
 
 # 处理缺失值
 # 处理 Age
