bc092d7a23f76b0391154260dbe4e22d429909b9
NicholasZXT <1036562425@qq.com>
2021-01-12 21:32:00 +0800

    BERT测试练习

--- Practice/00-data_science_practice.py
+++ Practice/00-data_science_practice.py
@@ -94,3 +94,11 @@ plt.show()
 
 
 # --------------sklearn---------------
+
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    print("name:", args[0])
+    print(args[1])
+    print(args)
