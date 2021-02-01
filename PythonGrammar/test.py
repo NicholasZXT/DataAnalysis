b86e43d8c24239bd033188c891ba814fa394ddf9
NicholasZXT <1036562425@qq.com>
2021-02-01 19:32:09 +0800

    python练习

--- PythonGrammar/test.py
+++ PythonGrammar/test.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from elasticsearch import Elasticsearch
+import configparser
 
 
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
@@ -16,6 +17,33 @@ data = {'title': '美国留给伊拉克的是个烂摊子吗', 'url': 'http://vi
 result = es.create(index='test', doc_type='_doc', id=1, body=data)
 
 es.cat.indices()
+
+
+# 获取路径中的最后一个文件夹名称
+os.path.basename('/path/to/Kaggle')
+# 获取文件夹路径
+os.path.dirname('/path/to/Kaggle')
+
+
+config = configparser.ConfigParser()
+config['DEFAULT'] = {'ServerAliveInterval': '45',
+                     'Compression': 'yes',
+                     'CompressionLevel': '9'}
+config['bitbucket.org'] = {}
+config['bitbucket.org']['User'] = 'hg'
+config['topsecret.server.com'] = {}
+topsecret = config['topsecret.server.com']
+topsecret['Port'] = '50022'     # mutates the parser
+topsecret['ForwardX11'] = 'no'  # same here
+config['DEFAULT']['ForwardX11'] = 'yes'
+with open('config.ini', 'w') as configfile:
+  config.write(configfile)
+
+
+config.sections()
+config.get(section='bitbucket.org', option='User')
+config.get(section='bitbucket.org', option='nothing', fallback='not exist')
+
 
 
 if __name__ == "__main__":
