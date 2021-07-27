01a851ece919d822526c1dff2a3cd43d5eb19730
NicholasZXT <1036562425@qq.com>
2021-07-27 20:09:06 +0800

    socket编程

--- Web-Development/socket_client.py
+++ Web-Development/socket_client.py
@@ -1,11 +1,42 @@
 import socket
+import selectors
 
-class Echo_client_v1:
-    def __init__(self, host, port):
+class EchoClientV1:
+    """
+    第一版，单个客户端连接
+    """
+    def __init__(self, host, port, input_flag=False):
         self.host = host
         self.port = port
+        self.__input_flag = input_flag
 
-    def run(self, word):
+    @property
+    def input_flag(self):
+        return self.__input_flag
+
+    @input_flag.setter
+    def input_flag(self, value):
+        if not isinstance(value, bool):
+            raise TypeError("Expect a bool value")
+        self.__input_flag = value
+
+    def run(self, word=None):
+        if self.input_flag:
+            self.run_input()
+        else:
+            self.run_word(word)
+
+    def run_input(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((self.host, self.port))
+            word = input("please input something: ")
+            print("Echo client sending words: ", word)
+            s.sendall(str.encode(word))
+            data = s.recv(1024)
+            print("Recieved: ", str(data, encoding='utf-8'))
+        print("Echo client stop")
+
+    def run_word(self, word=None):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             # 客户端的socket是主动式，它会调用 connect 方法
             s.connect((self.host, self.port))
@@ -15,8 +46,29 @@ class Echo_client_v1:
         print("Recieved: ", str(data, encoding='utf-8'))
 
 
+class EchoClientV2:
+    """
+    多客户端连接，使用 I/O多路复用 的方式
+    """
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sel = selectors.DefaultSelector()
+
+    def run(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((self.host, self.port))
+        sock.listen()
+        print("Echo server listening on {}:{}".format(self.host, self.port))
+        sock.setblocking(False)
+        self.sel.register(sock, selectors.EVENT_READ, data=None)
+        pass
+
+
 if __name__ == '__main__':
     host = "127.0.0.1"
     port = 3200
-    echo_client = Echo_client_v1(host, port)
-    echo_client.run("Hello world")
+    echo_client = EchoClientV1(host, port)
+    # echo_client.run("Hello world")
+    echo_client.input_flag = True
+    echo_client.run()
--- Web-Development/socket_server.py
+++ Web-Development/socket_server.py
@@ -1,6 +1,7 @@
 import socket
+import selectors
 
-class Echo_server_v1:
+class EchoServerV1:
     """
     echo server 第一版
     """
@@ -13,7 +14,7 @@ class Echo_server_v1:
             # 服务端的socket是被动式，它会使用如下的 bind, listen, accept 三个方法
             s.bind((self.host, self.port))
             s.listen()
-            print("echo server is listening...")
+            print("Echo server is listening...")
             # 调用此方法会阻塞，直到接收到客户端传送过来的新socket
             conn, addr = s.accept()
             with conn:
@@ -22,12 +23,26 @@ class Echo_server_v1:
                     data = conn.recv(1024)
                     if not data:
                         break
-                    print("echo server recieved: ", str(data, encoding='utf-8'))
+                    print("Echo server recieved: ", str(data, encoding='utf-8'))
                     conn.sendall(data)
+        print("Echo server stop.")
+
+
+class EchoServerV2:
+    """
+    多客户端连接，使用 I/O多路复用 的方式
+    """
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.sel = selectors.DefaultSelector()
+
+    def run(self):
+        pass
 
 
 if __name__ == '__main__':
     host = "127.0.0.1"
     port = 3200
-    echo_server = Echo_server_v1(host, port)
+    echo_server = EchoServerV1(host, port)
     echo_server.run()
