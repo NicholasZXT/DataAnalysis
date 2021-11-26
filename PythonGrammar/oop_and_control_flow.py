9d1ae7026904c5a84865a5ea739be7d3b31505c7
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2021-11-26 08:43:06 +0800

    协程练习

--- PythonGrammar/oop_and_control_flow.py
+++ PythonGrammar/oop_and_control_flow.py
@@ -7,7 +7,7 @@ def __Class_Practice():
     pass
 
 # 定义类
-class Person():
+class Person:
     address = 'China'
     def __init__(self,name,sex,weight,height):
         self.name = name
@@ -99,3 +99,56 @@ def add(x, y):
 def __Coroutine_Practice():
     pass
 
+# yield 的生成器用法
+def gen_fun():
+    # 通常 yield 会在一个循环里，不过不是必须的
+    print('start')
+    # 注意，此时 yield 后面跟了返回的值，左边是没有表达式的
+    yield 'A'
+    print('continue')
+    yield 'B'
+    print('end.')
+
+# if __name__ == '__main__':
+#     for v in gen_fun():
+#         print('--->', v)
+
+# yield 的协程用法
+def simple_coroutine():
+    print('-> coroutine start')
+    # 注意这里 yield 关键字的右边没有值，而左边有一个赋值表达式
+    # 实际上，yield 有两种含义：1. 将yield右边的值发送给调用方；2.yield停止的地方可以接受调用方传来的数据，yield将该数据赋值给左边的表达式
+    # 这里的 yield 后面没有值，只有左边有接收的表达式，说明此函数只接受数据，不产出数据
+    x = yield
+    print('-> coroutine received: ', x)
+
+# if __name__ == '__main__':
+#     my_coro = simple_coroutine()
+#     print('my_coro: ', my_coro)
+#     # 首先调用 next 方法，启动生成器，执行到 yield 处
+#     next(my_coro)
+#     # 执行到 yield 处之后，可以通过 send() 方法发送数据
+#     my_coro.send(12)
+#     # 这之后 yield 流程继续，直到最后抛出 StopIteration 异常
+
+# 使用协程来实现计算均值
+def coroutine_average():
+    sum = 0.0
+    count = 0
+    average = None
+    while True:
+        value = yield average
+        sum += value
+        count += 1
+        average = sum/count
+
+cor_average = coroutine_average()
+# 激活协程
+next(cor_average)
+# 计算均值
+cor_average.send(2)
+cor_average.send(3)
+cor_average.send(4)
+cor_average.send(5)
+# 关闭协程
+cor_average.close()
