fada8b09e9e9c52306a8d612166aae86df0b0328
NicholasZXT <1036562425@qq.com>
2021-12-24 16:39:13 +0800

    es客户端练习

--- PythonGrammar/elasticsearch_kafka.py
+++ PythonGrammar/elasticsearch_kafka.py
@@ -1,7 +1,15 @@
+import os
+import traceback
+from socket import timeout
+import argparse
+from time import sleep, time
+from datetime import datetime, timedelta
+import json
 import numpy as np
 import pandas as pd
-from elasticsearch import Elasticsearch, helpers
 from elasticsearch.exceptions import ConflictError, RequestError
+from elasticsearch import helpers, Elasticsearch, RequestError
+
 from kafka import KafkaClient, KafkaProducer, KafkaConsumer
 from kafka.errors import KafkaTimeoutError
 
@@ -124,6 +132,225 @@ res_parallel = helpers.parallel_bulk(es, actions, chunk_size=2, thread_count=2)
 
 res_iter = iter(res)
 item = next(res_iter)
+
+
+class EsClient:
+    def __init__(self, es_host):
+        self.es = Elasticsearch(es_host)
+
+    def query(self, index, condition_dict=None, size=500, query_dsl=None, df_format=True):
+        """
+        封装查询方式， 适用于小批量数据（不超过10000）
+        @param index:
+        @param condition_dict:
+        @param size: 默认500，最大不能超过10000
+        @param query_dsl: 自定义的ES查询语句
+        @param df_format: 是否转成DataFrame
+        @return:
+        """
+        if size == 500:
+            print(f"only return part of results with default size: {size}, please check size parameter.")
+        elif size > 10000:
+            print(f"parameter size is too large: {size}.")
+            raise IOError
+        else:
+            print(f"return results with limited size: {size}.")
+        condition_dict = condition_dict if condition_dict else {}
+        if query_dsl is None:
+            terms = [{'term': {k: v}} for k, v in condition_dict.items()]
+            query_dsl = {
+                'query': {
+                    'bool': {
+                        'must': terms
+                    }
+                }
+            }
+        res = self.es.search(index=index, body=query_dsl, request_timeout=20, size=size)
+        source_list = []
+        for hit in res['hits']['hits']:
+            source_list.append(hit['_source'])
+        res_final = None
+        if df_format:
+            res_final = pd.DataFrame(source_list)
+        else:
+            res_final = source_list
+        return res_final
+
+    def scroll_search(self, index, query_dict=None, query_dsl=None, size=500, scroll='5s'):
+        """
+        滚动查询大批量数据
+        @param index:
+        @param query_dict: 以 k-v 对 传入的条件
+        @param query_dsl: 自定义的查询DLS
+        @param size: 每批数据的大小
+        @param scroll: 滚动窗口的持续时间，字符串形式
+        @return:
+        """
+        _bool_query = dict()
+        condition_dict = query_dict if query_dict else {}
+        if query_dsl is None:
+            terms = [{'term': {k: v}} for k, v in condition_dict.items()]
+            query_dsl = {
+                'query': {
+                    'bool': {
+                        'must': terms
+                    }
+                }
+            }
+        try:
+            res = self.es.search(index=index, body=query_dsl, size=size, scroll=scroll)
+        except Exception as e:
+            print("failed to scroll search for index ‘{}’".format(index))
+            raise e
+        hits = res['hits']['hits']
+        total_value = res['hits']['total']['value']
+        i = 1
+        scroll_num = len(hits)
+        while hits:
+            yield hits
+            # print("scroll batch: ", i)
+            i += 1
+            res = self.es.scroll(scroll_id=res['_scroll_id'], scroll=scroll)
+            hits = res['hits']['hits']
+            # print("hits num: ", len(hits))
+            scroll_num += len(hits)
+        print("Scroll search total value is : {}, and scroll hits are : {}".format(total_value, scroll_num))
+
+    @staticmethod
+    def df2actions(df, index_name, id=False):
+        """
+        将 DataFrame 中的每条记录转成 action 格式，以便 ES helper 接口批量导入
+        @param df:
+        @param index_name: 要写入的 ES 索引名称
+        @return:list of dict, 每一个dict是一个action
+        @param id: 是否手动设置_id
+        """
+        count = 1
+        # 处理缺失值，np.nan 无法写入ES
+        df = df.where(pd.notnull(df), None)
+        for i, row in df.iterrows():
+            if count % 5000 == 0:
+                print(f'import data count is {count}')
+            count += 1
+            source = row.to_dict()
+            action = {
+                "_index": index_name,
+                "_type": '_doc',  # if self.es7 else 'medvol',
+                '_source': source
+            }
+            if id:
+                action['_id'] = source['LINE_ID'] + '-' + source['DATA_DATE']
+            yield action
+
+    def do_bulk(self, actions):
+        """
+        执行多线程批量导入ES
+        @param actions: 要导入的 ES actions，最好是生成器的形式
+        @return:
+        """
+        success = 0
+        failed = 0
+        write_success = True
+        while write_success and actions:
+            write_success = False
+            try:
+                for res in helpers.parallel_bulk(self.es, actions, chunk_size=50, request_timeout=10000, thread_count=2):
+                    # res 是一个 长度=2 的tuple，第一个元素是True或者False，表示是否导入成功
+                    if (success+1) % 10000 == 0:
+                        print("do_bulk running, success for {} records".format(success))
+                    if res[0]:
+                        success += 1
+                    else:
+                        failed += 1
+                        print('do_bulk failed res:%s' % res)
+            except timeout:
+                # 写入超时
+                print('bulk timeout and need retry')
+                actions = actions[success:]
+                sleep(60)
+                # 打开写入开关，在下一个while循环中继续写入
+                write_success = True
+            except Exception as e:
+                print('---------------Caught do_bulk exception-------------------')
+                print(e)
+                # traceback.print_exc()
+            else:
+                break
+        # 返回 写入成功的记录数、总记录数
+        print(f"bulk with success records: {success}, failed records: {failed}, total records: {success+failed}.")
+        return [success, success + failed]
+
+    def write_df(self, df, index, id=False):
+        """
+        将 pandas.DataFrame 批量导入 ES 中的指定索引，如果索引不存在，则创建一个新索引
+        @param df:
+        @param index:
+        @return:
+        """
+        actions = self.df2actions(df, index, id=id)
+        if self.es.indices.exists(index):
+            return self.do_bulk(actions)
+        else:
+            self.es.indices.create(index=index)
+            print("creating new index '{}'.".format(index))
+            return self.do_bulk(actions)
+
+    def export_data(self, index, query_dsl, path, scroll='10s'):
+        if not os.path.exists(path):
+            print(f"please check whether path ‘{path}’ existence.")
+            return None
+        file_path = os.path.join(path, index.strip("*") + '.csv')
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            print(f"file_path ‘{file_path}’ already exists and it was deleted !!!")
+        # source_list = []
+        records_iter = self.scroll_search(index=index, query_dsl=query_dsl, scroll=scroll)
+        sum = 0
+        for records in records_iter:
+            source_list = [record['_source'] for record in records]
+            df = pd.DataFrame(source_list)
+            if sum == 0:
+                df.to_csv(file_path, index=False)
+            else:
+                df.to_csv(file_path, mode='a', index=False, header=False)
+            sum += len(source_list)
+        # with open(file_path, 'a') as f:
+        #     for records in records_iter:
+        #         for record in records:
+        #             # f.write(json.dumps(record['_source']))
+        #             f.write(json.dumps(record['_source'], ensure_ascii=False))
+        #             f.write("\n")
+        #             sum += 1
+        print(f"export {sum} records to file: {file_path}.")
+
+    def reindex(self, index_map, source_host, source_user, source_pw, max_docs=20000):
+        body = {
+            "source": {
+                "remote": {
+                    "host": source_host,
+                    "username": source_user,
+                    "password": source_pw
+                },
+                "size": 1000,
+                "index": None
+            },
+            "dest": {
+                "index": None,
+                "op_type": "create"
+            }
+        }
+        res = {}
+        for k, v in index_map.items():
+            if not self.es.indices.exists(k):
+                print(f"target index {v} not exists, create it.")
+                self.es.indices.create(v)
+            print(f"reindex from '{k}' to '{v}'.")
+            body['source']['index'] = k
+            body['dest']['index'] = v
+            temp = self.es.reindex(body, timeout='1m')
+            res[k] = temp
+        return res
+
 
 
 # ========================== kafka ===========================
