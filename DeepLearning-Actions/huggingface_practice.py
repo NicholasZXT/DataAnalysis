c07c779a8abf7eb7e9ec0d619426662959dac594
NicholasZXT <55625521+NicholasZXT@users.noreply.github.com>
2022-10-31 00:29:26 +0800

    BERT在IMDB上使用MLM fine-tune完成

--- DeepLearning-Actions/huggingface_practice.py
+++ DeepLearning-Actions/huggingface_practice.py
@@ -1,20 +1,17 @@
 # %% importing packages
 import os
+from time import time
 import torch
-from torch import nn
-from torch.utils.data import Dataset, DataLoader
-from datasets import load_dataset_builder, get_dataset_config_names, get_dataset_config_info, \
-    get_dataset_infos, get_dataset_split_names,  load_dataset
+from torch import nn, optim
+from torch.utils.data import DataLoader
+from datasets import load_dataset_builder, get_dataset_config_names, get_dataset_split_names, load_dataset
 from tokenizers import Tokenizer, normalizers, pre_tokenizers, models, processors, trainers
-from transformers import DistilBertConfig, DistilBertTokenizer, DistilBertTokenizerFast, DistilBertForMaskedLM, \
-    DataCollatorForLanguageModeling
-from transformers import PreTrainedTokenizerFast
-from transformers import BertConfig, BertTokenizer, BertModel, BertForPreTraining, \
-    BertForSequenceClassification, DataCollatorForLanguageModeling
-
+from transformers import DataCollatorForLanguageModeling, DataCollatorForWholeWordMask, PreTrainedTokenizerFast
+from transformers import DistilBertConfig, DistilBertTokenizerFast, DistilBertForMaskedLM
+from transformers import BertConfig, BertTokenizer, BertTokenizerFast, BertModel, BertForMaskedLM, BertForPreTraining
 # 忽略警告信息
-import warnings
-warnings.filterwarnings('ignore')
+# import warnings
+# warnings.filterwarnings('ignore')
 
 # ***************** 基于 wikitext 语料，训练一个 Tokenizer *****************
 def __Train_tokenizer():
@@ -98,15 +95,24 @@ print(pair_encoding.attention_mask)
 def __Fine_Tune():
     pass
 
-# %% ----------- 加载 Distil-Bert 模型 -----------------
+# %% ----------- 加载 Bert 模型 -----------------
 base_path = r"D:\Project-Workspace\Python-Projects\DataAnalysis\bert-pretrained-models"
-model_path = os.path.join(base_path, 'distilbert-base-uncased')
-# tokenizer = DistilBertTokenizer.from_pretrained(model_path)
-tokenizer = DistilBertTokenizerFast.from_pretrained(model_path)
-config = DistilBertConfig.from_pretrained(model_path)
-# model = DistilBertModel(config)
-# 这里选择的是 Masked Language Model
-model_mlm = DistilBertForMaskedLM(config)
+# 这里采用 Masked Language Model 进行 fine-tune
+# BERT-base模型
+model_path = os.path.join(base_path, 'bert-base-uncased')
+tokenizer = BertTokenizerFast.from_pretrained(model_path)
+config = BertConfig.from_pretrained(model_path)
+# 需要手动设置输出 attentions 和 hidden_states
+# config.output_attentions = True
+# config.output_hidden_states = True
+model = BertModel(config)
+model_mlm = BertForMaskedLM(config)
+# Distil-Bert
+# model_path = os.path.join(base_path, 'distilbert-base-uncased')
+# tokenizer = DistilBertTokenizerFast.from_pretrained(model_path)
+# config = DistilBertConfig.from_pretrained(model_path)
+# model_mlm = DistilBertForMaskedLM(config)
+
 # 查看信息
 # print(model_mlm.num_parameters())
 # print(tokenizer.__class__)
@@ -120,6 +126,7 @@ data_name = r'.\datasets\huggingface\imdb.py'
 print(get_dataset_config_names(data_name))
 print(get_dataset_split_names(data_name))
 imdb = load_dataset(data_name, split='unsupervised')
+print(imdb)
 
 # %% --------------- 处理数据 -----------------------
 # 对于 Masked Language Model，在准备训练语料的时候，通常的做法是将 一个batch里 所有样本的文本段落拼在一起，然后按照固定长度分成 chunk，用这些
@@ -134,17 +141,20 @@ def tokenize_function(examples):
     # examples 是以 batch 传入的样本
     # result 是分词后的结果(BatchEncoding对象)，包括 input_ids 和 attention_mask 两个key
     result = tokenizer(examples["text"])
-    if tokenizer.is_fast:
+    # 如果后面直接使用 DataCollatorForWholeWordMask，那就不需要这个 word_ids
+    # if tokenizer.is_fast:
         # 新增一个key，记录下每个batch的句子里，各个token的下标
-        result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))]
+        # result["word_ids"] = [result.word_ids(i) for i in range(len(result["input_ids"]))]
     return result
 
 # 使用 Dataset 对象的 map 方法，以 batch 方式处理
-imdb_tokenized = imdb.map(tokenize_function, batched=True, remove_columns=["text", "label"])
-print(imdb_tokenized)
+imdb_tokenized = imdb.map(tokenize_function, batched=True, remove_columns=["text", "label"], load_from_cache_file=True)
+# 可以看出，样本数量没变，但是 features 由 ['text', 'label'] --> ['input_ids', 'attention_mask', 'word_ids']
+# print(imdb_tokenized)
 
 # 2. 分词结束后，需要将 一个batch 里的所有样本拼接起来，然后按照固定长度(chunk_size)分割成 chunk
 chunk_size = 128
+# chunk_size = 16
 # 使用下面的函数完成操作
 def group_texts(examples):
     # examples 是一个 batch 的样本，keys()返回的是 input_ids, attention_mask, word_ids
@@ -152,7 +162,7 @@ def group_texts(examples):
     # sum函数签名为 sum(Iterable, start=0)，examples[k] 是一个 List[List], 使用 sum 时，会对其中的 子list 执行 + ，也就是list的拼接，
     # 最后的结果再 + start，由于 start默认是整数0，无法和 list 执行 +， 所以这里使用了一个空列表
     concatenated_examples = {k: sum(examples[k], []) for k in examples.keys()}
-    # 计算拼接后的总长度，list(examples.keys())[0] 就是 input_ids 这个key
+    # 计算此 batch 的样本拼接后的总长度，list(examples.keys())[0] 就是 input_ids 这个key
     total_length = len(concatenated_examples[list(examples.keys())[0]])
     # 对于最后一个chunk，它的长度可能小于 chunk_size，这里直接丢弃
     total_length = (total_length // chunk_size) * chunk_size
@@ -162,25 +172,102 @@ def group_texts(examples):
         k: [t[i: i + chunk_size] for i in range(0, total_length, chunk_size)]
         for k, t in concatenated_examples.items()
     }
-    # Create a new labels column —— 这个 label 后面会被作为被 mask 词的预测标签
+    # Create a new labels column —— 这个 label 后面会作为 mask 词的预测标签
     result["labels"] = result["input_ids"].copy()
     return result
 
-imdb_chunks = imdb_tokenized.map(group_texts, batched=True)
+imdb_chunks = imdb_tokenized.map(group_texts, batched=True, load_from_cache_file=True)
 # 这里可以看出，样本个数已经比原来的 50000 要多了
 print(imdb_chunks)
+# 查看一下样本内容
+# print(imdb_chunks['input_ids'][0])
+# print(tokenizer.decode(imdb_chunks['input_ids'][0]))
+# print(imdb_chunks['labels'][0])
+# print(tokenizer.decode(imdb_chunks['labels'][0]))
+
 
 # 3. 对数据集进行分割 chunk 之后，还需要每个chunk中的 token 进行随机 mask，这一步是通过 DataCollatorForLanguageModeling 完成的
-# 使用 tokenizer 初始化 DataCollator 对象
-imdb_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
+# 使用 tokenizer 初始化 DataCollator 对象，因为要使用 tokenizer 中的特殊token来进行mask
+# 下面这个只能 mask 分词后的 subword 对应的 token
+# lm_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
+# 下面这个可以 mask 分词后，多个 subword 对应的 token 组成的 完整word，使用这个时，不需要上面生成的 word_ids
+lm_collator = DataCollatorForWholeWordMask(tokenizer=tokenizer, mlm_probability=0.15)
 # 可以查看下效果
-samples = [imdb_chunks[i] for i in range(2)]
-# 去除掉其中的 word_ids，因为 collator 不接受这个特征
-for sample in samples:
-    _ = sample.pop("word_ids")
-res = imdb_collator(samples)
+# samples = [imdb_chunks[i] for i in range(2)]
+# 去除掉其中的 word_ids，因为word_ids的开头或者结尾是 None，无法转成 tensor，会报错
+# for sample in samples:
+#     _ = sample.pop("word_ids")
+# res = lm_collator(samples)
+# res 是 transformers.tokenization_utils_base.BatchEncoding 对象
+# print(res.__class__)
 # 查看 Mask 的效果
-tokenizer.decode(res['input_ids'][0])
+# print(res['input_ids'][0])
+# print(tokenizer.decode(res['input_ids'][0]))
+# labels 中，除了被 mask 的 token， 其他位置的 token 都被转成了 -100
+# print(res['labels'][0])
+
+# 4. 将上述的数据封装成 pytorch 的 DataLoader
+imdb_dataloader = DataLoader(dataset=imdb_chunks, batch_size=16, shuffle=True, collate_fn=lm_collator)
+
+
+# %% ---------------- Fine-Tune 模型 ------------------------
+# 查看下MLM模型输出
+# test_batch_size = 4
+# sample = [imdb_chunks[i] for i in range(test_batch_size)]
+# sample_batch = lm_collator(sample)
+# with torch.no_grad():
+#     output = model(sample_batch['input_ids'])
+#     output_mlm = model_mlm(**sample_batch)
+# print(type(output), '; ', type(output_mlm))
+# # 序列中每个 token 的词向量长度由 BERT 的配置决定，同时词典中的token个数决定了MLM输出的向量维度
+# print(f"config.hidden_size: {config.hidden_size}, config.vocab_size: {config.vocab_size}")
+# # 输入一个 batch 的数据，样本数为 test_batch_size，每个样本中序列长度为 chunk_size
+# print(sample_batch['input_ids'].shape)
+# # labels 的形状和输入的 input_ids 是一样的
+# print(sample_batch['labels'].shape)
+#
+# # 经过 Bert 模型后的隐藏层输出为 (batch_size,seq_length, hidden_size), 其中 seq_length = chunk_size
+# print(output.last_hidden_state.shape)
+# # print(output.attentions[-1].shape)
+# # print(output.hidden_states[-1].shape)
+# # Bert模型最后一层的输出，经过 BertOnlyMLMHead 转换后，每个 token 的维度从 hidden_size --> vocab_size，也就是对应于 vocab 中各个词的概率
+# print(output_mlm.logits.shape)
+# # 直接输出一个 batch 下，所有样本序列的 交叉熵损失之和，可以直接使用
+# print(output_mlm.loss)
+
+
+# GPU 设置
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+model_mlm.cuda(device)
+# print(device)
+# 定义优化器
+optimizer = optim.SGD(params=model_mlm.parameters(), lr=0.1)
+# 进行训练
+num_train_epochs = 10
+loss = 0
+print(f"training on device: {device}")
+start_time = time()
+for epoch in range(1, num_train_epochs+1):
+    model_mlm.train()
+    for batch_num, batch in enumerate(imdb_dataloader, start=1):
+        input_ids, labels = batch['input_ids'], batch['labels']
+        input_ids = input_ids.to(device)
+        labels = labels.to(device)
+        optimizer.zero_grad()
+        # outputs = model_mlm(**batch)
+        outputs = model_mlm(input_ids=input_ids, labels=labels)
+        loss = outputs.loss
+        if batch_num % 100 == 0:
+            print(f"training loss at batch '{batch_num}' of epoch '{epoch}' is: {loss}")
+        loss.backward()
+        optimizer.step()
+    print(f"training loss finally at epoch '{epoch}' is : {loss}")
+end_time = time()
+total_time = end_time - start_time
+print(f"total training time is : {total_time}")
+
+# RTX 2070 Super， batch_size=30, chunk_size=128，1个epoch耗时 1574.86秒
+
 
 
 
@@ -207,7 +294,6 @@ data_collator = DataCollatorForLanguageModeling()
 base_path = r"D:\Project-Workspace\Python-Projects\DataAnalysis\bert-pretrained-models"
 model_path = os.path.join(base_path, "bert-base-uncased")
 config = BertConfig.from_pretrained(pretrained_model_name_or_path=model_path)
-tokenizer = BertTokenizer.from_pretrained(pretrained_model_name_or_path=model_path, local_files_only=True)
 bert_basic = BertModel.from_pretrained(pretrained_model_name_or_path=model_path, local_files_only=True)
 bert_train = BertForPreTraining.from_pretrained(pretrained_model_name_or_path=model_path, local_files_only=True)
 # bert_cls = BertForSequenceClassification.from_pretrained(pretrained_model_name_or_path=model_path, local_files_only=True)
