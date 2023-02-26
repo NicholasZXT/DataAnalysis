4ead43b47cc84d2d49d17006743888c6b59ce547
NicholasZXT <1036562425@qq.com>
2023-02-26 22:38:18 +0800

    工业蒸汽量预测

---
+++ PracticingProjects/Industry-Steam-Prediction/steam.py
@@ -0,0 +1,13 @@
+import os
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+
+DATA_DIR = r"D:\Project-Workspace\Python-Projects\DataAnalysis\local-datasets\工业蒸汽量预测"
+train_data = pd.read_csv(os.path.join(DATA_DIR, 'zhengqi_train.txt'), delimiter='\t', header=0)
+test_data = pd.read_csv(os.path.join(DATA_DIR, 'zhengqi_test.txt'), delimiter='\t', header=0)
+X = train_data.drop(columns=['target'])
+y = train_data['target'].copy()
+
+
