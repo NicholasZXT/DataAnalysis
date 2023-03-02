948f7feb335bd50d1e092f12f72daabba054021f
NicholasZXT <1036562425@qq.com>
2023-03-02 17:24:15 +0800

    工业蒸汽量预测EDA

--- PracticingProjects/Industry-Steam-Prediction/steam.py
+++ PracticingProjects/Industry-Steam-Prediction/steam.py
@@ -9,8 +9,8 @@ plt.style.use("ggplot")
 from sklearn.linear_model import LinearRegression
 
 # %% 数据加载
-DATA_DIR = r"D:\Project-Workspace\Python-Projects\DataAnalysis\local-datasets\工业蒸汽量预测"
-# DATA_DIR = r"C:\Users\Drivi\Python-Projects\DataAnalysis\local-datasets\工业蒸汽量预测"
+# DATA_DIR = r"D:\Project-Workspace\Python-Projects\DataAnalysis\local-datasets\工业蒸汽量预测"
+DATA_DIR = r"C:\Users\Drivi\Python-Projects\DataAnalysis\local-datasets\工业蒸汽量预测"
 train_data = pd.read_csv(os.path.join(DATA_DIR, 'zhengqi_train.txt'), delimiter='\t', header=0)
 test_data = pd.read_csv(os.path.join(DATA_DIR, 'zhengqi_test.txt'), delimiter='\t', header=0)
 X = train_data.drop(columns=['target'])
@@ -26,45 +26,11 @@ test_data_desc = test_data.describe()
 
 
 # *********** 单变量分析 *************
-# %% 检查每个特征的分布，使用IQR查看异常值情况
-# 这里使用seaborn绘图，需要将数据转成long-format，每5个变量一组绘图展示
-def df_split(df: pd.DataFrame, split_num: int = 5):
-    """
-    对df中的列，按照 split_num 一组，进行分隔，然后 wide-to-long，用于seaborn绘图
-    """
-    col_num = df.shape[1]
-    splits = col_num // split_num
-    dfs = []
-    for i in range(splits):
-        start = i * split_num
-        df_part = pd.melt(X, value_vars=['V'+str(i) for i in range(start, start+split_num)], var_name='V')
-        dfs.append(df_part)
-    final_start = splits * split_num
-    df_part = pd.melt(X, value_vars=['V'+str(i) for i in range(final_start, col_num)], var_name='V')
-    dfs.append(df_part)
-    return dfs
-
-def df_plot(dfs: List[pd.DataFrame]):
-    """
-    每个df绘制一个Figure
-    """
-    num = len(dfs)
-    figs = []
-    for i in range(num):
-        fig = plt.figure(num=i+1, figsize=(8.0, 5.0), dpi=120)
-        ax = fig.add_subplot(111)
-        # sns.stripplot(data=dfs[i], x='value', y='V', ax=ax)
-        sns.boxplot(data=dfs[i], x='value', y='V', ax=ax)
-        # sns.boxenplot(data=dfs[i], x='value', y='V', ax=ax)
-        # sns.violinplot(data=dfs[i], x='value', y='V', ax=ax)
-        figs.append(fig)
-        # fig.show()
-        # fig.clear()
-    return figs
-
+# %% 检查每个特征的分布，绘制每个特征的箱线图
+# 多个特征的箱线图放到同一个图形里的方式不太好，因为各个特征的量纲会相互影响，改为采用每个特征一个子图的方式绘制，保留每个特征自己的量纲
 def box_single_check(df: pd.DataFrame, V: str, show=True):
     """
-    着重检查某个变量的图形
+    着重检查某个变量的箱线图和小提琴图
     """
     fig = plt.figure(figsize=(8.0, 6.0), dpi=120)
     ax1 = fig.add_subplot(211)
@@ -75,17 +41,18 @@ def box_single_check(df: pd.DataFrame, V: str, show=True):
         fig.show()
     return fig
 
-def box_subplot(df: pd.DataFrame, rows: int = 4, cols: int = 2):
+def box_subplot(df: pd.DataFrame, rows: int = 4, cols: int = 2, w=6.8, h=4.8):
     """
-    每个变量一个子图，每个Figure绘制一个 rows x cols 的图
+    每个变量一个子图，每个Figure绘制一个 rows x cols 布局的箱线图
     """
-    figsize = (16.0, 8.0)
+    figsize = (w, h)
     col_num = df.shape[1]
     col_num_per_fig = rows * cols
     fig_num = col_num // col_num_per_fig
     figs = []
     for i in range(fig_num):
-        fig = plt.figure(num=i+1, figsize=figsize, dpi=120)
+        # 注意设置 clear=True， 否则可能拿到的是已经使用过的Figure
+        fig = plt.figure(num='box-'+str(i+1), figsize=figsize, dpi=100, clear=True, layout='tight')
         for j in range(col_num_per_fig):
             feature_num = i * col_num_per_fig + j
             feature = 'V' + str(feature_num)
@@ -98,7 +65,7 @@ def box_subplot(df: pd.DataFrame, rows: int = 4, cols: int = 2):
         figs.append(fig)
     cols_remain = col_num % col_num_per_fig
     final_rows = int(np.ceil(cols_remain / cols))
-    final_fig = plt.figure(num=fig_num, figsize=figsize, dpi=120)
+    final_fig = plt.figure(num='box-'+str(fig_num+1), figsize=figsize, dpi=100, clear=True, layout='tight')
     # print(f"col_num: {col_num}, cols_remain: {cols_remain}")
     for j in range(cols_remain):
         feature_num = col_num - cols_remain + j
@@ -112,28 +79,25 @@ def box_subplot(df: pd.DataFrame, rows: int = 4, cols: int = 2):
     figs.append(final_fig)
     return figs
 
-# %% 绘制每个特征的箱线图，使用IQR过滤异常值
-# 下面这种多个变量放到同一个图形里的方式不太好，因为各个变量的量纲会相互影响
-# dfs = df_split(X, split_num=6)
-# figs = df_plot(dfs)
-# 改为采用每个变量一个子图的方式绘制，保留每个变量自己的量纲
-figs = box_subplot(df=X, rows=4, cols=2)
-figs[0].show()
-figs[1].show()
-figs[2].show()
-figs[3].show()
-figs[4].show()
+# %% 分析每个特征的箱线图
+box_figs = box_subplot(df=X, rows=4, cols=3, w=16, h=10)
+box_figs[0].show()
+box_figs[1].show()
+box_figs[2].show()
+box_figs[3].show()
+box_figs[4].show()
 # 着重检查某个变量
-fig_single = box_single_check(X, 'V9')
+box_fig_single = box_single_check(X, 'V36')
 # fig_single.show()
 # 检查结果
 # V9 的量纲范围比较大，有几个异常值需要剔除
-# V10, V12, V15, V16, V17, V19, V20,    也要异常值需要剔除
+# V10, V12, V15, V16, V17, V19, V20 有异常值需要剔除
 # V23, V25, V30, V31, V33, V34, V35   的分布过于离散，超出 IQR 的样本比较多
 # V28, V29, V36, 也需要考虑
 
-# %% 使用IQR作为异常值判断准则，检查每个特征有多少个异常值
+# %% 使用IQR作为异常值判断准则，检查每个特征有多少个异常值，进行异常值过滤
 # IQR 的阈值，默认1.5
+# 这里为了避免删除太多样本点，IQR阈值放宽到了 3.0 !!!
 iqr_th = 3.0
 X_q1 = X.quantile(q=0.25)
 X_q3 = X.quantile(q=0.75)
@@ -147,7 +111,6 @@ X_iqr_outlier = iqr_max_flag | iqr_min_flag
 X_col_outlier_num = X_iqr_outlier.sum(axis=0)
 # 可以看出 V9, V23, V34 的异常值（1.5阈值下）都比较多：835, 700, 488
 # 阈值增大到 1.8，V9, V23, V34 的异常值为：833, 678, 450
-# 这里为了避免删除太多观测点，IQR阈值放宽到了 3.0 !!!
 X_delete_rows = X_iqr_outlier.sum(axis=1) > 0
 # 查看需要删除的样本数量，阈值3.0下，需要删除 771 个样本
 print(X_delete_rows.sum())
@@ -157,7 +120,7 @@ X_no_outlier = X.loc[~X_delete_rows, :]
 # t = (X_no_outlier < X_iqr_min) | (X_no_outlier > X_iqr_max)
 
 
-# %% 绘制每个变量的直方图和Q-Q图，查看分布的偏度
+# %% 绘制每个特征的直方图和Q-Q图，查看分布的偏度
 def skew_subplot(df, cols=4, w=6.4, h=4.8):
     """
     对每个特征绘制直方图和Q-Q图，以上、下两行的形式放在同一列，cols指定的是每一幅图中绘制的特征个数
@@ -167,7 +130,8 @@ def skew_subplot(df, cols=4, w=6.4, h=4.8):
     fig_num = col_num // cols
     figs = []
     for i in range(fig_num):
-        fig = plt.figure(num=i+1, figsize=figsize)
+        # 注意设置 clear=True， 否则可能拿到的是上面已经使用过的Figure
+        fig = plt.figure(num='skew-'+str(i+1), figsize=figsize, dpi=100, clear=True, layout='tight')
         for j in range(cols):
             feature_num = i * cols + j
             feature_name = 'V' + str(feature_num)
@@ -182,12 +146,13 @@ def skew_subplot(df, cols=4, w=6.4, h=4.8):
             ax1.set_title(feature_name)
             ax1.set_xlabel('')
             ax2.set_xlabel('')
-        fig.tight_layout()
+        # 上面使用了 tight，这里可以不必使用了
+        # fig.tight_layout()
         figs.append(fig)
     col_remain = col_num % cols
     if col_remain == 0:
         return figs
-    final_fig = plt.figure(num=fig_num+1, figsize=figsize)
+    final_fig = plt.figure(num='skew-'+str(fig_num+1), figsize=figsize, dpi=100, clear=True, layout='tight')
     for j in range(col_remain):
         feature_num = fig_num * cols + j
         feature_name = 'V' + str(feature_num)
@@ -220,20 +185,38 @@ def skew_single_check(df, feature,  w=6.4, h=4.8, show=False):
     return fig
 
 
-# %% 检查各个特征的直方图和Q-Q图
+# %% 分析各个特征的直方图和Q-Q图
+# 使用没有删除异常值的数据绘图的话，也能发现有些特征偏离的比较厉害
+# skew_figs = skew_subplot(X, cols=5, w=25, h=10)
 # cols_used = ['V'+str(i) for i in range(10)]
-# figs = skew_subplot(X_no_outlier[cols_used], cols=5, w=25, h=10)
-figs = skew_subplot(X_no_outlier, cols=5, w=25, h=10)
-figs[0].show()
-figs[1].show()
-figs[2].show()
-figs[3].show()
-figs[4].show()
-figs[5].show()
-figs[6].show()
-figs[7].show()
+# skew_figs = skew_subplot(X_no_outlier[cols_used], cols=5, w=25, h=10)
+skew_figs = skew_subplot(X_no_outlier, cols=5, w=25, h=10)
+skew_figs[0].show()
+skew_figs[1].show()
+skew_figs[2].show()
+skew_figs[3].show()
+skew_figs[4].show()
+skew_figs[5].show()
+skew_figs[6].show()
+skew_figs[7].show()
 # 检查单个变量
-fig = skew_single_check(X_no_outlier, feature='V9', w=10, h=5, show=True)
+# skew_fig_single = skew_single_check(X, feature='V9', w=10, h=5, show=True)
+skew_fig_single = skew_single_check(X_no_outlier, feature='V34', w=10, h=5, show=True)
 # 检查结果为：
-# V0, V1, V5, V6, V7, V11, V14, V16 偏离正态较严重，需要后续做变换处理
+# V0, V1, V5, V6, V7, V8, V11, V14, V16, V18 偏离正态较严重，需要后续做变换处理
 # V9, V17, V22, V23, V24, V28, V35 要特别注意，似乎取值只有几个离散的值
+# V33, V34 这两个变量中间的值有跳变
+
+
+# %% 对比训练数据和测试数据各个特征的分布情况，绘制KDE图
+fig = plt.figure()
+ax = fig.add_subplot(111)
+sns.kdeplot(x=X_no_outlier['V1'], ax=ax, color='yellow')
+sns.kdeplot(x=test_data['V1'], ax=ax, color='r')
+fig.show()
+
+def kde_subplot(train_df, test_df, rows=4, cols=4, w=6.4, h=4.8):
+    figsize = (w, h)
+    col_num = train_df.shape[1]
+    fig_num = col_num // cols
+    figs = []
