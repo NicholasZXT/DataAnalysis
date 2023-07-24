199e2fee3a09b18df70c0ed6804b09bc1a3c4438
NicholasZXT <1036562425@qq.com>
2023-07-24 13:42:07 +0800

    删除django_web和flask_web的文件夹

--- Web-Development/django_web/hello_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-
-# Create your views here.
-def hello_index(response):
-    return HttpResponse('Hello, this is django hello index.')
